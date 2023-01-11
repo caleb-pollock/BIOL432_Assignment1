a286e424a7ef67efd6b0f1c2e7a0c20df86fe44b
caleb-pollock <calebpollock77@gmail.com>
2023-01-11 12:51:10 -0500

    Edited and fixed the code to prevent volume error from occuring

--- dataGenerato.R
+++ dataGenerato.R
@@ -1,13 +1,17 @@
 #code for organism, samples five names 100 times and has the ability to replace
+
+#set a seed for reproducibility 
+set.seed(7127)
+
 Organism <- sample(c("Homo sapiens","Homo erectus","Homo neanderthalensis","Denisova hominins","Homo luzonensis"),100,replace = T)
 Organism
 
 #code for limb width, random normal distribution with mean of 10 and sd of 2
-Limb_Width <- rnorm(100, mean = 10, sd = 2)
+Limb_Width <- abs(rnorm(100, mean = 10, sd = 2))
 Limb_Width
 
 #code for limb height, random normal distribution with mean of 79 and sd of 15
-Limb_Height <- rnorm(100, mean = 79, sd = 15)
+Limb_Height <- abs(rnorm(100, mean = 79, sd = 15))
 Limb_Height
 
 #code for observer, samples three names 100 times and has the ability to replace 
@@ -15,8 +19,9 @@ Observer <- sample(c("Caleb P","Kennedy R","Louie Y"),100,replace = T)
 Observer
 
 #combines all vectors into one data frame
-measurements <- data.frame(Organism,Limb_Width,Limb_Width,Observer)
+measurements <- data.frame(Organism,Limb_Width,Limb_Height,Observer)
 measurements
 
 #writes the data frame to a CSV file
 write.csv(measurements, "C:\\Users\\Caleb\\Desktop\\Winter 2023 Stats\\measurements.csv",row.names = F)
+View(data)
--- volumeEstimato.R
+++ volumeEstimato.R
@@ -4,13 +4,12 @@ library(dplyr)
 #imports measurements.csv data into script
 data <- read.csv("C:\\Users\\Caleb\\Desktop\\Winter 2023 Stats\\measurements.csv")
 
-#calculates radius from limb width
-radius = data$Limb_Width/2
 
 #data set is overwritten and mutated to include a new column with volume, calculated through 3.14*r^2*h
 data <- data %>%
-  mutate(Volume = pi * radius^2 * data$Limb_Height)
+  mutate(Volume = pi * (data$Limb_Width/2)^2 * data$Limb_Height)
 
 #measurements.csv is updated with new column
 write.csv(data, "measurements.csv",row.names = F)
+View(data)
 
